657cce6c1ccb958c80787ce153ecfd9ed7a42bd5
Sanchit <97388765+sanchit180@users.noreply.github.com>
2022-03-19 10:25:46 +0530

    Add files via upload

---
+++ MILESTONE 1/Ques_5MultiplicationOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2,num3;
+    cout<<"Enter first number:";
+    cin>>num1;
+    cout<<"Enter second number:";
+    cin>>num2;
+    num3=num1*num2;
+    cout<<"multiplication of two number is:";
+    cout<<num3;
+    return 0;
+    }
